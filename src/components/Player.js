6318942fd48a9334aab7a3703e18e284204a2737
ImanRS1 <omid-omega@mail.com>
2021-02-08 10:38:03 +0100

    Added player component

---
+++ src/components/Player.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
