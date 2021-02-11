3ef8db39ef77c2e9a6788a08486fe688cb6d12b0
ImanRS1 <omid-omega@mail.com>
2021-02-11 14:50:44 +0100

    added fix for NaN display on switch song

--- src/components/Player.js
+++ src/components/Player.js
@@ -84,7 +84,7 @@ const Player = ({
           max={songInfo.duration || 0}
           type="range"
         />
-        <p>{getTime(songInfo.duration)}</p>
+        <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon
