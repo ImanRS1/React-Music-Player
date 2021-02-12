9c1e76adc4bb7fbf0df82d772f411f73ab5f313f
ImanRS1 <omid-omega@mail.com>
2021-02-12 12:09:05 +0100

    added mquery style and animation to player and lib

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav setLibraryStatus={setLibraryStatus} libraryStatus={libraryStatus} />
       <Song currentSong={currentSong} />
       <Player
